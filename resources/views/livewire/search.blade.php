8719b318c814228ff2a7d84b532155a0206cd619
Renee Magallanes <rene22797@gmail.com>
2021-08-07 21:56:23 -0500

    Seccion 11 finalizada

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,6 +1,26 @@
-<div class="flex-1 relative">
-    <x-jet-input type="text" class="w-full" placeholder="¿Estas buscando algun producto?" />
-    <button class="bg-orange-500 absolute right-0 top-0 w-12 h-full flex items-center justify-center rounded-r-md">
-        <x-search/>
-    </button>
+<div class="flex-1 relative" x-data>
+    <form action="{{ route('search') }}" autocomplete="off">
+        <x-jet-input name="name" wire:model="search" type="text" class="w-full" placeholder="¿Estas buscando algun producto?" />
+        <button class="bg-orange-500 absolute right-0 top-0 w-12 h-full flex items-center justify-center rounded-r-md">
+            <x-search />
+        </button>
+    </form>
+    <div @click.away="$wire.open = false" :class="{'hidden': !$wire.open}" class="absolute w-full hidden">
+        <div class="bg-white rounded-lg shadow mt-1">
+            <div class="px-4 py-3 space-y-1">
+                @forelse ($products as $product)
+                    <a href="{{ route('products.show', $product) }}" class="flex">
+                        <img src="{{ Storage::url($product->images->first()->url) }}" alt=""
+                            class="w-16 h-12 object-cover">
+                        <div class="ml-4 text-gray-700">
+                            <p class="text-lg font-semibold leading-5">{{ $product->name }}</p>
+                            <p>Categoría: {{ $product->subcategory->category->name }}</p>
+                        </div>
+                    </a>
+                @empty
+                    <p class="text-lg leading-5">No existe el producto indicado</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
 </div>
