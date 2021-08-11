d75a3aa64b0094c063f10760ae324227ca867705
Renee Magallanes <rene22797@gmail.com>
2021-08-11 18:01:37 -0500

    Seccion 15 finalizada

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\WebhooksController;
 use App\Http\Livewire\CreateOrder;
 use App\Http\Livewire\ShoppingCart;
 
@@ -33,4 +34,10 @@ Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
 /*middleware solo para usuarios identificados*/
 Route::get('orders/create', CreateOrder::class)->middleware('auth')->name('orders.create');
 
+Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+
 Route::get('orders/{order}/payment', [OrderController::class, 'payment'])->name('orders.payment');
+
+Route::get('orders/{order}/pay', [OrderController::class, 'pay'])->name('orders.pay');
+
+Route::post('webhooks', WebhooksController::class);
